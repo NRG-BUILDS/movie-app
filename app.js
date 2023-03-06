cea38525ae51e2cc545acbae73cb41117eb69849
Emmanuel Omolaju <111093908+NRG-BUILDS@users.noreply.github.com>
2023-03-06 15:15:41 +0100

    Bug fixes

--- app.js
+++ app.js
@@ -1,126 +1,102 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '693844bab5msh01c0a96d3111ac8p13869ajsn656d8e45f937',
-		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-	}
-};
-const options2 = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '693844bab5msh01c0a96d3111ac8p13869ajsn656d8e45f937',
-		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-	}
-};
+fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd')
+.then(response => response.json())
+.then(response => showTrending(response))
+.catch(err => console.error(err));
+
+showTrending = (data) => { 
+    console.log(data);
+    const list = data.results;
+    let picArray = []
+    let text = ""
+    const display = document.querySelector('.trending');
+    display.innerHTML = ""
+    for(let i = 0; i < data.results.length; i++) { 
+        let picSrc = "https://image.tmdb.org/t/p/w500" + list[i].poster_path;
+        let name = list[i].original_title;
+        let overview = list[i].overview;
+        
+        picArray.push(picSrc);
+        text += `<div>
+              <img src="${picArray[i]}" class="movie_pic">
+              <div class="slide_info">
+                  <span class="title">${name}</span>
+                  <span class="genre">Action</span>
+                  <span class="trend_number">Trending #${i+1}</span>
+                  <span class="brief_synopsis">${overview}</span>
+              </div>
+          </div>`
+    }
+    display.innerHTML += text;
+}
+
+
+fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd')
+.then(response => response.json())
+.then(response => showNewMovies(response))
+.catch(err => console.error(err));
+
+showNewMovies = (data) => { 
+    console.log(data);
+    const list = data.results;
+    let picArray = []
+    let text = ""
+    const display = document.querySelector('.new_movies');
+    display.innerHTML = ""
+    for(let i = 0; i < data.results.length; i++) { 
+        let picSrc = "https://image.tmdb.org/t/p/w500" + list[i].poster_path;
+        let name = list[i].original_title;
+        let vote = list[i].vote_average;
+        
+        picArray.push(picSrc);
+        text += `<div>
+                  <img src="${picArray[i]}" class="movie_pic">
+                  <div class="slide_info">
+                      <span class="genre">${name}<br>${vote}</span>
+                  </div>
+              </div>`
+    }
+    display.innerHTML += text;
+}
+
+
+
+
+fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd')
+.then(response => response.json())
+.then(response => showUpcoming(response))
+.catch(err => console.error(err));
+
+showUpcoming = (data) => { 
+    console.log(data);
+    const list = data.results;
+    let picArray = []
+    let text = ""
+    const display = document.querySelector('.upcoming');
+    display.innerHTML = ""
+    for(let i = 0; i < data.results.length; i++) { 
+        let picSrc = "https://image.tmdb.org/t/p/w500" + list[i].poster_path;
+        let name = list[i].original_title;
+        let releaseDate = list[i].release_date;
+        
+        picArray.push(picSrc);
+        text += `<div>
+                  <img src="${picArray[i]}" class="movie_pic">
+                  <div class="slide_info">
+                      <span class="genre">${name}<br>${releaseDate}</span>
+                  </div>
+              </div>`
+    }
+    display.innerHTML += text;
+}
+
+
+
 const searchOption = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '9291656858mshef3c555eb2e1bf1p18be2bjsnde3c88b7d459',
 		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
 	}
-};
-fetch('https://online-movie-database.p.rapidapi.com/title/get-most-popular-movies?currentCountry=US&purchaseCountry=US&homeCountry=US', options)
-	.then(response => response.json())
-	.then(response => getKeys(response))
-	.catch(err => console.error(err));
-fetch('https://online-movie-database.p.rapidapi.com/title/get-coming-soon-movies?currentCountry=US&purchaseCountry=US&homeCountry=US', options)
-	.then(response => response.json())
-	.then(response => getUpcomingKeys(response))
-	.catch(err => console.error(err));	
-
-let trendKeysArray = []	
-getKeys = (response) => { 
-    
-    for (i = 0; i < 10; i++) { 
-        let lastLetter = response[i].length - 1;
-        trendKeysArray[i] = response[i].slice(7, lastLetter);
-    }
-    getTrendingData();
-}
-
-getTrendingData = () => { 
-    let key =trendKeysArray.join("%2C")
-    fetch(`https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=${key}`, options2)
-	.then(response => response.json())
-	.then(data => showTrending(data))
-	.catch(err => console.error(err));
-}
-showTrending = (data) => { 
-    trend = 1;
-    const list = data.results
-    let picArray = [];
-    let i = 0
-    for(let i = 0; i < data.results.length; i++) { 
-        if ( data.results[i].primaryImage === null) { 
-            console.log(data)
-        } else { 
-            console.log(data.results[i].primaryImage.url)
-            picArray.push(data.results[i].primaryImage.url);
-        }
-        
-    }
-    list.map((item) => {
-        name = item.titleText.text;
-        
-        text = `<div>
-              <img src="${picArray[i]}" class="movie_pic">
-              <div class="slide_info">
-                  <span class="title">${name}</span>
-                  <span class="genre">Action</span>
-                  <span class="trend_number">Trending #${trend}</span>
-                  <span class="brief_synopsis">Read More...</span>
-              </div></div>`
-        display = document.querySelector('.trending');
-        display.innerHTML += text;
-        trend++;
-        i++;
-        })
-};
-
-let upcomingKeysArray = []	
-getUpcomingKeys = (response) => { 
-    
-    for (i = 0; i < 10; i++) { 
-        let lastLetter = response[i].id.length - 1;
-       upcomingKeysArray[i] = response[i].id.slice(7, lastLetter);
-    }
-    getUpcomingData();
-}
-getUpcomingData = () => { 
-    let key = upcomingKeysArray.join("%2C")
-    fetch(`https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=${key}`, options2)
-	.then(response => response.json())
-	.then(data => showUpcoming(data))
-	.catch(err => console.error(err));
-    
-}
-showUpcoming = (data) => { 
-    const list = data.results
-    
-    let picArray = [];
-        let i = 0
-        for (let i = 0; i < 5; i++) {
-            if ( data.results[i].primaryImage === null) { 
-            console.log(data)
-        } else { 
-            console.log(data.results[i].primaryImage.url)
-            picArray.push(data.results[i].primaryImage.url)
-        }
-        
-        }    
-    list.map((item) => {
-        //console.log(item)
-        name = item.titleText.text;
-        date = item.releaseDate.day + '-' +item.releaseDate.month + '-' +item.releaseDate.year
-        text = `<div>
-                  <img src="${picArray[i]}" class="movie_pic">
-                  <div class="slide_info">
-                      <span class="genre">${name}:<br>${date}</span>
-                  </div>
-              </div>>`
-        display = document.querySelector('.upcoming');
-        display.innerHTML += text
-        })
 };
 searchMovies = () => { 
     let query = document.querySelector('#search_bar').value;
