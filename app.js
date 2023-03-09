1d7dd23bef832f6678dc8af3e124a4277b65163b
Emmanuel Omolaju <111093908+NRG-BUILDS@users.noreply.github.com>
2023-03-09 13:58:41 +0100

    Improved search feature

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ showTrending = (data) => {
               <img src="${picArray[i]}" class="movie_pic">
               <div class="slide_info">
                   <span class="title">${name}</span>
-                  <span class="genre">Action</span>
+                  <span class="genre"><span class="material-icons-outlined">star</span>${vote}</span>
                   <span class="trend_number">Trending #${i+1}</span><button class="trend_div"><a class='trend_link_btn'><span class='material-icons-outlined'>arrow_forward</span></a></button>
                   <span class="brief_synopsis">${overview}</span>
               </div>
@@ -148,6 +148,53 @@ showUpcoming = (data) => {
 
 
 
+fetch('https://api.themoviedb.org/3/tv/popular?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd')
+.then(response => response.json())
+.then(response => showTopSeries(response))
+.catch(err => console.error(err));
+
+showTopSeries = (data) => { 
+    //console.log(data);
+    const list = data.results;
+    let picArray = []
+    let topSeriesObj = {
+        name: [],
+        id: [],
+        bg: [],
+        overview: [],
+        vote: []
+    }
+    let text = ""
+    const display = document.querySelector('.top_series');
+    display.innerHTML = ""
+    for(let i = 0; i < data.results.length; i++) { 
+        let picSrc = "https://image.tmdb.org/t/p/w500" + list[i].poster_path;
+        let name = list[i].name;
+        let vote = list[i].vote_average;
+        let bgSrc = "https://image.tmdb.org/t/p/w1280" + list[i].backdrop_path
+        let movie_id = list[i].id
+        let overview = list[i].overview;
+        
+        
+        topSeriesObj.name[i] = name;
+        topSeriesObj.id[i] = movie_id;
+        topSeriesObj.bg[i] = bgSrc;
+        topSeriesObj.overview[i] = overview;
+        topSeriesObj.vote[i] = vote;
+        
+        picArray.push(picSrc);
+        text += `<div>
+                  <img src="${picArray[i]}" class="movie_pic">
+                  <div class="slide_info">
+                      <span class="genre">${name}<br>${vote}</span><button class="trend_div"><a class='series_link_btn'><span class='material-icons-outlined'>arrow_forward</span></a></button>
+                  </div>
+              </div>`
+    }
+    display.innerHTML += text;
+    createTopSeriesButton(topSeriesObj);
+}
+
+
 const searchOption = {
 	method: 'GET',
 	headers: {
@@ -157,167 +204,148 @@ const searchOption = {
 };
 searchMovies = () => { 
     let query = document.querySelector('#search_bar').value;
-    fetch(`https://imdb8.p.rapidapi.com/title/find?q=${query}`, searchOption)
+    fetch(`https://api.themoviedb.org/3/search/multi?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd&language=en-US&query=${query}&page=1&include_adult=true`)
         .then(response => response.json())
-        .then(data => showResults(data))
+        .then(data => showResults(data, query))
         .catch(err => console.error(err));
 }
-showResults = (data) => { 
+showResults = (data, query) => { 
     const list = data.results
     let displayContainer = document.querySelector('.search_results_container');
     let display = document.querySelector('.search_results')
+    document.querySelector(`#query_display`).innerHTML = query;
     let otherDisplay = document.querySelector('.main_sections');
     let picArray = [];
-    let i = 0
-    for(let i = 0; i < 5; i++) { 
-        //console.log(data.results[i].image.url)
-        picArray.push(data.results[i].image.url)
+    let searchResObj = {
+        name: [],
+        id: [],
+        bg: [],
+        overview: [],
+        vote: [],
+        type: []
     }
-    console.log(picArray)
-    list.map((items) => { 
-        name = items.title;
-        pic = items.image.url;
-        years = items.year;
-        text = `<div>
+    let i = 0;
+    let text = ""
+    for(let i = 0; i < 5; i++) { 
+        let name;
+        let pic = "https://image.tmdb.org/t/p/w500" + list[i].poster_path;
+        let movie_id = list[i].id
+        let overview = list[i].overview
+        let vote = list[i].vote_average
+        let bgSrc = "https://image.tmdb.org/t/p/w500" + list[i].backdrop_path
+        picArray.push(pic)
+        let years 
+        if(list[i].media_type == "movie") { 
+            years = list[i].release_date.slice(0, 4);
+            name = list[i].title
+        } else {
+            years = list[i].first_air_date.slice(0, 4);
+            name = list[i].name
+        }
+        let media_type = list[i].media_type
+        
+        searchResObj.name[i] = name;
+        searchResObj.id[i] = movie_id;
+        searchResObj.bg[i] = bgSrc;
+        searchResObj.overview[i] = overview;
+        searchResObj.vote[i] = vote;
+        searchResObj.type[i] = media_type
+        
+        text += `<div>
                   <img src="${picArray[i]}" class="movie_pic">
                   <div class="slide_info">
                       <span class="title-sm">${name}</span>
-                      <span class="genre">${years}</span>
+                      <span class="genre">${years}</span><button class="trend_div"><a class='search_link_btn'><span class='material-icons-outlined'>arrow_forward</span></a></button>
                   </div>
               </div>`
-        displayContainer.style.display = "block";
-        otherDisplay.style.display = "none";
-        display.innerHTML += text;
-        i++;
-    })
-}
-showDetails = (trendObj, id_key) => { 
-    console.log(trendObj)
-    console.log(trendObj.id[0])
-    const homeScreen = document.querySelector('.home')
-    const detailsScreen = document.querySelector('.details_page')
-    const bgDisplay = document.querySelector('.backdrop_pic')
-    const ttDisplay = document.querySelector('.detail_title')
-    const detDisplay = document.querySelector('.details_container')
-    for (let i = 0; i < trendObj.id.length; i++) { 
-        if (trendObj.id[i] == id_key) { 
-        fetch(`https://api.themoviedb.org/3/movie/${trendObj.id[i]}/credits?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd`)
-        .then(response => response.json())
-        .then(data => showCast(data))
-        .catch(err => console.error(err));
-        fetch(`https://api.themoviedb.org/3/movie/${trendObj.id[i]}?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd`)
-        .then(response => response.json())
-        .then(data => { 
-            detDisplay.innerHTML = `<div class="details_container">
-              <p>•${data.genres[0].name}-${data.genres[1].name}•${getTime(data.runtime)}</p>
-              <p>${gradeVote(trendObj.vote[i])}</p>
-              <p>${trendObj.overview[i]}</p>
-          </div>`
-        })
-        .catch(err => console.error(err));
-        
-        
-        bgDisplay.src = trendObj.bg[i];
-        ttDisplay.innerHTML = `<span class="title">${trendObj.name[i]}</span>`
-        
-        homeScreen.style.display = "none";
-        detailsScreen.style.display ="block"
-        }
-    
-    } 
-    
-}
-showCast = (data) => { 
-    const list = data.cast;
-    const display = document.querySelector('.actor_tray')
-    
-    let text = `<div>
-                  <div>
-                      <img src="${"https://image.tmdb.org/t/p/w500"+list[0].profile_path}" class="actor_pic">
-                      <span>${list[0].name}</span>
-                  </div>
-                  <div>
-                      <img src="${"https://image.tmdb.org/t/p/w185"+list[1].profile_path}" class="actor_pic">
-                      <span>${list[1].name}</span>
-                  </div>
-              </div>
-              <div>
-                  <div>
-                      <img src="${"https://image.tmdb.org/t/p/w185"+list[2].profile_path}" class="actor_pic">
-                      <span>${list[2].name}</span>
-                  </div>
-                  <div>
-                      <img src="${"https://image.tmdb.org/t/p/w185"+list[3].profile_path}" class="actor_pic">
-                      <span>${list[3].name}</span>
-                  </div>
-              </div>
-              <div>
-                  <div>
-                      <img src="${"https://image.tmdb.org/t/p/w185"+list[4].profile_path}" class="actor_pic">
-                      <span>${list[4].name}</span>
-                  </div>
-                  <div>
-                      <img src="${"https://image.tmdb.org/t/p/w185"+list[5].profile_path}" class="actor_pic">
-                      <span>${list[5].name}</span>
-                  </div>
-              </div>`
-    display.innerHTML = text
-}
-gradeVote = (num) =>  { 
-    let stars = Math.floor(num / 2)
-    let halfStar = num % 2;
-    let text = ""
-    for (let i = 0; i < stars; i++) { 
-        text +=`<span class="material-icons-outlined">star</span>`
     }
-    if (halfStar >= 0.5) { 
-        text += `<span class="material-icons-outlined">star_half</span>`
-    }
-    text += `(${num})`
-    return text
+    displayContainer.style.display = "block";
+    otherDisplay.style.display = "none";
+    display.innerHTML = text;
+    i++;
+    createSearchResButton(searchResObj)
 }
-getTime = (time) => { 
-    let hr = Math.floor(time / 60);
-    let min = time % 60
-    let text = `${hr}h${min}m`
-    
-    return text
+closeSearch = () => { 
+    let displayContainer = document.querySelector('.search_results_container');
+    let otherDisplay = document.querySelector('.main_sections');
+    otherDisplay.style.display = "block"
+    displayContainer.style.display = "none"
 }
-
 createTrendButton = (trendObj) => { 
-    console.log(trendObj)
+    
     let btn = document.querySelectorAll('.trend_link_btn');
     for (let i = 0; i < 20; i++) { 
         btn[i].href = "movie.html"
         btn[i].addEventListener("click", 
         function() { 
+            let type = "movie"
             sessionStorage.setItem("trendObj", JSON.stringify(trendObj));
-        sessionStorage.setItem("id_key", trendObj.id[i]) }
+            sessionStorage.setItem("id_key", trendObj.id[i]) 
+            sessionStorage.setItem("item_type", type)
+        }
+            
         )
     }
 }
 createNewMoviesButton = (trendObj) => { 
-     console.log(trendObj)
+     
     let btn = document.querySelectorAll('.nwmov_link_btn');
     for (let i = 0; i < 20; i++) { 
         btn[i].href = "movie.html"
         btn[i].addEventListener("click", 
         function() { 
+            let type = "movie"
             sessionStorage.setItem("trendObj", JSON.stringify(trendObj));
-        sessionStorage.setItem("id_key", trendObj.id[i]) }
+            sessionStorage.setItem("id_key", trendObj.id[i]) 
+            sessionStorage.setItem("item_type", type)
+        }
+        
         )
     }
 }
 createUpcomingButton = (trendObj) => { 
-     console.log(trendObj)
+     
     let btn = document.querySelectorAll('.upcoming_link_btn');
     for (let i = 0; i < 20; i++) { 
         btn[i].href = "movie.html"
         btn[i].addEventListener("click", 
         function() { 
+            let type = "movie"
             sessionStorage.setItem("trendObj", JSON.stringify(trendObj));
-        sessionStorage.setItem("id_key", trendObj.id[i]) }
+            sessionStorage.setItem("item_type", type);
+            sessionStorage.setItem("id_key", trendObj.id[i]) 
+            
+        }
         )
     }
 }
-
+createTopSeriesButton = (trendObj) => { 
+    
+    let btn = document.querySelectorAll('.series_link_btn');
+    for (let i = 0; i < 20; i++) { 
+        btn[i].href = "movie.html"
+        btn[i].addEventListener("click", 
+        function() { 
+            let type = "tv"
+            sessionStorage.setItem("trendObj", JSON.stringify(trendObj));
+            sessionStorage.setItem("id_key", trendObj.id[i]) 
+            sessionStorage.setItem("item_type", type)
+        }
+        )
+    }
+}
+createSearchResButton = (trendObj) => { 
+     
+    let btn = document.querySelectorAll('.search_link_btn');
+    for (let i = 0; i < 20; i++) { 
+        btn[i].href = "movie.html"
+        btn[i].addEventListener("click", 
+        function() { 
+            sessionStorage.setItem("trendObj", JSON.stringify(trendObj));
+            sessionStorage.setItem("item_type", trendObj.media_type[i]);
+            sessionStorage.setItem("id_key", trendObj.id[i]) 
+            
+        }
+        )
+    }
+}
--- details.js
+++ details.js
@@ -1,28 +1,38 @@
 let trendObj = JSON.parse(sessionStorage.getItem("trendObj"))
 let id_key = sessionStorage.getItem("id_key")
+let item_type = sessionStorage.getItem("item_type")
+console.log(item_type)
+checkItemType = () => { 
+    if (item_type == "movie") { 
+        showMovieDetails(trendObj, id_key, item_type)
+    } else { 
+        showSeriesDetails(trendObj, id_key, item_type)
 
-showDetails = (trendObj, id_key) => {
+    }
+}
+checkItemType()
+function showMovieDetails(trendObj, id_key, item_type) {
     const detailsScreen = document.querySelector('.details_page')
     const bgDisplay = document.querySelector('.backdrop_pic')
     const ttDisplay = document.querySelector('.detail_title')
     const detDisplay = document.querySelector('.details_container')
     for (let i = 0; i < trendObj.id.length; i++) {
         if (trendObj.id[i] == id_key) {
-            fetch(`https://api.themoviedb.org/3/movie/${trendObj.id[i]}/credits?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd`)
+            fetch(`https://api.themoviedb.org/3/${item_type}/${trendObj.id[i]}/credits?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd`)
                 .then(response => response.json())
-                .then(data => showCast(data))
+                .then(data => showMovieCast(data))
                 .catch(err => console.error(err));
             fetch(`https://api.themoviedb.org/3/movie/${trendObj.id[i]}?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd`)
                 .then(response => response.json())
                 .then(data => {
                     detDisplay.innerHTML = `<div class="details_container">
-              <p>•${data.genres[0].name}-${data.genres[1].name}•${getTime(data.runtime)}</p>
+              <p>${data.release_date.slice(0, 4)}•${getGenre(data.genres)}•${getTime(data.runtime)}</p>
               <p>${gradeVote(trendObj.vote[i])}</p>
               <p>${trendObj.overview[i]}</p>
               </div>`
                 })
                 .catch(err => console.error(err));
-            fetch(`https://api.themoviedb.org/3/movie/${trendObj.id[i]}/videos?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd`)    
+            fetch(`https://api.themoviedb.org/3/${item_type}/${trendObj.id[i]}/videos?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd`)    
             .then(response => response.json())
             .then(data => { 
                 console. log(data)
@@ -37,7 +47,61 @@ showDetails = (trendObj, id_key) => {
     }
 
 }
-showCast = (data) => {
+
+function showSeriesDetails(trendObj, id_key, item_type) {
+    const detailsScreen = document.querySelector('.details_page')
+    const bgDisplay = document.querySelector('.backdrop_pic')
+    const ttDisplay = document.querySelector('.detail_title')
+    const detDisplay = document.querySelector('.details_container')
+    for (let i = 0; i < trendObj.id.length; i++) {
+        if (trendObj.id[i] == id_key) {
+            fetch(`https://api.themoviedb.org/3/${item_type}/${trendObj.id[i]}/credits?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd`)
+                .then(response => response.json())
+                .then(data => showTVCast(data))
+                .catch(err => console.error(err));
+            fetch(`https://api.themoviedb.org/3/${item_type}/${trendObj.id[i]}?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    detDisplay.innerHTML = `<div class="details_container">
+              <p>${data.first_air_date.slice(0, 4)}•${getGenre(data.genres)}•${data.number_of_episodes} episodes</p>
+              <p>${gradeVote(data.vote_average)}</p>
+              <p>${data.overview}</p>
+              </div>`
+                })
+                .catch(err => console.error(err));
+            fetch(`https://api.themoviedb.org/3/${item_type}/${trendObj.id[i]}/videos?api_key=66ffe7fe1fbf9c25e7766e730cd29fcd`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    const display = document.querySelector('.youtube_container')
+                    const video = data.results[1].key;
+                    display.src = `https://www.youtube.com/embed/${video}`
+                })
+            bgDisplay.src = trendObj.bg[i];
+            ttDisplay.innerHTML = `<span class="title">${trendObj.name[i]}</span>`
+        }
+    }
+}
+showTVCast = (data) => {
+    const list = data.cast;
+    const display = document.querySelector('.actor_tray')
+
+    let text = `<div>
+                  <div>
+                      <img src="${"https://image.tmdb.org/t/p/w500"+list[0].profile_path}" class="actor_pic">
+                      <span>${list[0].name}</span>
+                  </div>
+                  <div>
+                      <img src="${"https://image.tmdb.org/t/p/w185"+list[1].profile_path}" class="actor_pic">
+                      <span>${list[1].name}</span>
+                  </div>
+              </div>
+              
+              </div>`
+    display.innerHTML = text
+}
+showMovieCast = (data) => {
     const list = data.cast;
     const display = document.querySelector('.actor_tray')
 
@@ -53,7 +117,7 @@ showCast = (data) => {
               </div>
               <div>
                   <div>
-                      <img src="${"https://image.tmdb.org/t/p/w185"+list[2].profile_path}" class="actor_pic">
+                      <img src="${"https://image.tmdb.org/t/p/w500"+list[2].profile_path}" class="actor_pic">
                       <span>${list[2].name}</span>
                   </div>
                   <div>
@@ -62,14 +126,24 @@ showCast = (data) => {
                   </div>
               </div>
               <div>
+                                    <img src="${"https://image.tmdb.org/t/p/w500"+list[4].profile_path}" class="actor_pic">
+                                    <span>${list[4].name}</span>
+                                </div> <
+              div >
+                  <img src="${"https://image.tmdb.org/t/p/w185"+list[5].profile_path}" class="actor_pic">
+                                    <span>${list[5].name}</span> <
+                  /div> <
+                  /div>
                   <div>
-                      <img src="${"https://image.tmdb.org/t/p/w185"+list[4].profile_path}" class="actor_pic">
-                      <span>${list[4].name}</span>
-                  </div>
-                  <div>
-                      <img src="${"https://image.tmdb.org/t/p/w185"+list[5].profile_path}" class="actor_pic">
-                      <span>${list[5].name}</span>
-                  </div>
+                                        <img src="${"https://image.tmdb.org/t/p/w500"+list[6].profile_path}" class="actor_pic">
+                                        <span>${list[6].name}</span>
+                                    </div> <
+                  div >
+                      <img src="${"https://image.tmdb.org/t/p/w185"+list[7].profile_path}" class="actor_pic">
+                                        <span>${list[7].name}</span> <
+                      /div> <
+                      /div>
+
               </div>`
     display.innerHTML = text
 }
@@ -93,4 +167,13 @@ getTime = (time) => {
 
     return text
 }
-showDetails(trendObj, id_key)
+getGenre = (array) => { 
+    let text = ""
+    if (array.length === 0) { text = ""} else { 
+        for (i = 0; i < array.length; i++) { 
+            text += `${array[i].name}-`
+        }
+    }
+    
+    return text
+}
